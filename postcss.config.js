c9ce6e85e0aa35843e7fd5e3f38ebeeeeb70d92b
Dion <diome.halcyonwebdesign@gmail.com>
2022-10-18 13:12:27 +0800

    postbuild 1

--- postcss.config.js
+++ postcss.config.js
@@ -20,7 +20,7 @@ module.exports = {
               './pages/**/*.{js,jsx,ts,tsx}',
               './components/**/*.{js,jsx,ts,tsx}',
               './node_modules/bootstrap/dist/css/bootstrap.min.css'
-          ],
+          ], 
           defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
           safelist: ["html", "body"]
         }
